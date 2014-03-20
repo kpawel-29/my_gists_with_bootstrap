95625d62e7f8880897bcf8a9757782675b28a752
kpawel-29 <pakadlubowski@gmail.com>
2014-03-20 20:01:33 +0100

    push my project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyGists::Application.config.secret_key_base = '88dd455df10838adb19912609f3df39dc4caa3e1a1d49107cd787598000eaff38bae708b1c77a1097791437174806a79cd54c40b6aefabaacdea9f82cb27daf3'
